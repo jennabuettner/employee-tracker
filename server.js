6f733494281585b84b1704744982606284057a83
jennabuettner <jenna.e.buettner@gmail.com>
2022-07-03 11:56:22 -0700

    wrote server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const inquirer = require("./config/inquirer");
+const sql = require("mysql2");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const dbConnect = sql.createConnection(
+  {
+    host: "localhost",
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+  },
+  console.log("Connection to database has been established.")
+);
+
+module.exports = dbConnect;
