36220db0dbc10d6c3dbd9d3489677c06b71cd25e
jennabuettner <jenna.e.buettner@gmail.com>
2022-07-03 14:55:00 -0700

    fixed schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,30 +4,26 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name: VARCHAR(30)
-)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE role (
-    id: INT PRIMARY KEY AUTO_INCREMENT PRIMARY KEY,
-    title: VARCHAR(30),
-    salary: DECIMAL,
-    departmend_id: INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCES department(id),
-    ON DELETE SET NULL,
-)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
 
 CREATE TABLE employee (
-    id: INT PRIMARY KEY AUTO_INCREMENT PRIMARY KEY,
-    first_name: VARCHAR(30),
-    last_name: VARCHAR(30),
-    role_id: INT NOT NULL
-    manager_id: INT NOT NULL
-    CONSTRAINT (fk_manager),
-    FOREIGN KEY (manager_id)
-    REFERENCES (employee_id),
-    FOREIGN KEY (role_id)
-    REFERENCES role(id),
-    ON DELETE SET NULL
-)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT, 
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INT,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
