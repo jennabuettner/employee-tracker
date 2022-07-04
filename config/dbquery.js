60dda68cfaf1a531ea8d4322edb9d2b6733bf11b
jennabuettner <jenna.e.buettner@gmail.com>
2022-07-04 08:33:49 -0700

    switching things around

--- config/dbquery.js
+++ config/dbquery.js
@@ -1,33 +1,76 @@
-const db = require('');
+const db = require('./connection');
 const dbConnection = require('../server');
-const allTables = ('console.table')
+const allTables = ('console.table');
 
-function queries() {
-    function displayDept() {
-        const sql = `SELECT * FROM departments;`;
-        db.query(sql, (params), (err,data) => {
-            console.table(table)
-        })
-    };
+addEmployee(employee) {
+    const sql = `INSERT INTO employees(firstname, lastname, role) VALUES (?, ?, ?);`;
+    db.query(sql, (params), (err,data) => {
+        console.log(table)
+    })
+}
 
-    function displayEmployees() {
-        const sql = `SELECT * FROM employees;`;
-        db.query(sql, (params), (err,data) => {
-            console.table(table)
-        })
-    };
+function displayDept() {
+    const sql = `SELECT * FROM department`;
+    db.query(sql, (params), (err,data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('\n');
+            console.table(data);
+            console.log('\n');
+        }
+        console.log('-------------------------')
+        mainSplash();
+    });
+}
 
-    function displayRoles() {
-        const sql = `SELECT * FROM role;`;
-        db.query(sql, (params), (err,data) => {
-            console.log(table)
-        }) 
-    };
 
-    addEmployee(employee) {
-        const sql = `INSERT INTO employees(firstname, lastname, role) VALUES (?, ?, ?);`;
-        db.query(sql, (params), (err,data) => {
-            console.log(table)
-        })
-    };
-};
+function displayEmployees() {
+    const sql = `SELECT employee.id, 
+    employee.first_name AS First, employee.last_name AS Last, 
+    role.title AS Title, 
+    role.salary AS Salary, 
+    department.name AS Department, 
+    CONCAT(manager.first_name, '', manager.last_name) AS Manager 
+    FROM employee employee 
+    LEFT JOIN employee manager 
+    ON employee.manager_id = manager.id, 
+    JOIN department 
+    ON role.department_id = department.id, 
+    JOIN role ON employee.role_id = role.id;`;
+    db.query(sql, (params), (err,data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('\n');
+            console.table(data);
+            console.log('\n');
+        }
+        console.log('-------------------------')
+        mainSplash();
+    });
+}
+
+function displayRoles() {
+    const sql = `SELECT 
+    role.title,
+    role.id,
+    role.salary,
+    department.name
+    FROM role
+    LEFT JOIN department
+    ON role.department_id = department.id;`;
+    db.query(sql, function (err, data) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('\n');
+            console.table(data);
+            console.log('\n');
+        }
+        console.log('~~~~~~~~~~~~~~~~~~~~~~');
+        mainSplash();
+    });
+}
+
+    mainSplash()
